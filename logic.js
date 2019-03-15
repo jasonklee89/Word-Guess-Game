4cf8f41e58a12df06b022497d622b7cbed19d22e
Jason Lee <jasonlee@dhcp-v101-180.mobile.uci.edu>
2019-03-15 10:01:13 -0700

    created logicjs page and pseudocoded the function

---
+++ logic.js
@@ -0,0 +1,30 @@
+// Global Variables
+var cookieBank = ["snickerdoodle", "chocolatechip", "oatmealraisin", "gingersnap", "shortbread", "peanutbutter", "sugar", "biscotti", "butter", "macaroon", "fortune", "gingerbread"]
+var selectedCookie = "";
+var lettersInCookie = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+// Functions
+
+function start () {
+    // select word
+    // break word apart into individual letters
+    // get number of blanks
+
+    // Reset
+        // another set of guesses
+        // wrong guesses back to 0
+        // blanks and successes clear
+    
+    // populate blanks and successes with right number of blanks
+
+    // Testing
+}
+
+// Main Process
