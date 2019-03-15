397e83b5632e04eef5e58dabc75b90ad46c830bb
Jason Lee <jasonlee@dhcp-v101-180.mobile.uci.edu>
2019-03-15 12:46:13 -0700

    added and tested first parts of logic

--- logic.js
+++ logic.js
@@ -8,23 +8,41 @@ var wrongLetters = [];
 
 var winCount = 0;
 var lossCount = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
 
 // Functions
 
-function start () {
+function start() {
     // select word
+    selectedCookie = cookieBank[Math.floor(Math.random() * cookieBank.length)];
     // break word apart into individual letters
+    lettersInCookie = selectedCookie.split("");
     // get number of blanks
+    numBlanks = lettersInCookie.length;
 
     // Reset
-        // another set of guesses
-        // wrong guesses back to 0
-        // blanks and successes clear
+    // another set of guesses
+    guessesLeft = 10;
+    // wrong guesses back to 0
+    wrongLetters = [];
+    // blanks and successes clear
+    blanksAndSuccesses = [];
     
     // populate blanks and successes with right number of blanks
+    for (i = 0; i > lettersInCookie.length; i++) {
+        
+    }
 
     // Testing
+    console.log(selectedCookie);
+    console.log(lettersInCookie);
+    console.log(numBlanks);
+    console.log(guessesLeft);
+    console.log(wrongLetters);
+    console.log(blanksAndSuccesses);
+
 }
+
+start();
 
 // Main Process
